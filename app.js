21da2536fcc8d97caf9d1d5d843fe297cd702480
Zachary Siegel <siegelzc@gmail.com>
2018-10-15 18:19:31 -0500

    remove bootstrap.min.js: not in use

--- app.js
+++ app.js
@@ -15,21 +15,21 @@ app.set("views", "./src/views");
 app.set("view engine", "pug");
 
 
-console.log("Listening on " + port);
+console.log(`Listening on port ${port}`);
 
 // Sockets
 let cons = 0;
 io.on('connection', socket => {
    cons++;
-   console.log('Client Connected (' + cons + ')');
+   console.log(`Client Connected (${cons})`);
 
    // Send Reports
    db.getLostReports((reports) => {
-      socket.emit('Lost Reports', reports ? reports : []);
+       socket.emit('Lost Reports', reports ? reports : []);
    });
 
    socket.on('disconnect', () => {
-      cons--;
-      console.log('Client Disconnected (' + cons + ')');
+       cons--;
+       console.log(`Client Disconnected (${cons})`);
    });
 });
